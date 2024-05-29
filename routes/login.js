9088968084082350d76360b1c5308dbdd5dc9ae7
Aditya Jain <jainaditya0411@gmail.com>
2024-05-30 01:18:02 +0530

    add jwt on login

--- routes/login.js
+++ routes/login.js
@@ -2,40 +2,54 @@ var express = require('express');
 var router = express.Router();
 var dbConn = require('../config/db');
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
 
 // render login jade page
-router.get('/', function(req, res, next) {
-  res.render('login', {bottom_text:''});
+router.get('/', function (req, res, next) {
+  res.render('login', {
+    bottom_text: ''
+  });
 });
 
 // login auth
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
-    username = req.body.username;
-    password = req.body.password;
+  username = req.body.username;
+  password = req.body.password;
 
-    dbConn.query("SELECT * FROM users WHERE username = ?", [username], function(err, result) {
-        if (err) {
-            res.render('login', {bottom_text:'Error in database query'});
-            return;
-        }
-        if (result.length === 0) {
-            res.render('login', {bottom_text:'Invalid username or password'});
-            return;
-        }
-        var user = result[0];
-        console.log(user);
-        bcrypt.compare(password, user.password).then(function(result) {
-            if (result) {
-                res.render('login', {bottom_text:'Login successful'});
-            } else {
-                res.render('login', {bottom_text:'Invalid username or password'});
-            }
+  dbConn.query("SELECT * FROM users WHERE username = ?", [username], function (err, result) {
+    if (err) {
+      res.render('login', {
+        bottom_text: 'Error in database query'
+      });
+      return;
+    }
+    if (result.length === 0) {
+      res.render('login', {
+        bottom_text: 'Invalid username or password'
+      });
+      return;
+    }
+    var user = result[0];
+    console.log(user);
+    bcrypt.compare(password, user.password).then(function (result) {
+      if (result) {
+        const token = jwt.sign({
+          username: username,
+          admin: user.isadmin
+        }, process.env.JWT_SECRET);
+        res.cookie('jwt', token).status(200);
+        res.redirect("../");
+      } else {
+        res.render('login', {
+          bottom_text: 'Invalid username or password'
         });
+      }
     });
+  });
 
 });
-  
+
 module.exports = router;
