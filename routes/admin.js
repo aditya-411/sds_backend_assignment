7316a3c718147af028c9478638409e7e8ba9776d
Aditya Jain <jainaditya0411@gmail.com>
2024-06-05 00:21:02 +0530

    add update admin options page

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,9 @@ var router = express.Router();
 var auth = require('../config/auth').admin;
 var dbConn = require('../config/db');
 
-/* GET users listing. */
 router.get('/', auth, function (req, res, next) {
   res.render('admin_home', {
     username: req.user.username,
-    isadmin: true
   });
 });
 
