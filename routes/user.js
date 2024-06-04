281c96267fe6d76fa20a9427d6d86f7054876eb3
Aditya Jain <jainaditya0411@gmail.com>
2024-06-04 23:02:24 +0530

    return functionality added

--- routes/user.js
+++ routes/user.js
@@ -139,54 +139,82 @@ router.get('/my_books', auth, function (req, res, next) {
 
 
 router.post('/return', auth, function (req, res, next) {
-  dbConn.query("SELECT * FROM pending_approvals WHERE username = ? AND title = ? AND type = false", [req.user.username, req.body.title], function (err, result) {
+  dbConn.query("SELECT * FROM books", function (err, booksResult) {
     if (err) {
       res.render('error', {
-        error: err,
-        message: err.message
+        error: err
       });
       return;
     }
-    if (result.length > 0) {
-      console.log(req.body.allBooks);
-      res.render('view_books_user', {
-        book: req.body,
-        message: 'Return approval already pending for this book',
-        currentlyIssuedBooks: req.body.currentlyIssuedBooks,
-        bookHistory: req.body.bookHistory,
-        allBooks: req.body.allBooks,
-        pendingBooks: req.body.pendingBooks
-
-      });
-      return;
-    }
-    dbConn.query("INSERT INTO pending_approvals (title, username, type) VALUES (?, ?, false)", [req.body.title, req.user.username], function (err, result) {
+    dbConn.query("SELECT * FROM currently_issued WHERE username = ?", [req.user.username], function (err, issuedResult) {
       if (err) {
         res.render('error', {
-          error: err,
-          message: err.message
+          error: err
         });
         return;
       }
-      dbConn.query("SELECT * FROM pending_approvals WHERE username = ?", [req.user.username], function (err, pendingResult) {
+      dbConn.query("SELECT * FROM history WHERE username = ?", [req.user.username], function (err, historyResult) {
         if (err) {
           res.render('error', {
             error: err
           });
           return;
         }
-        console.log(pendingResult);
-        res.render('view_books_user', {
-          message: 'Return approval requested successfully',
-          pendingBooks: pendingResult,
-          currentlyIssuedBooks: req.body.currentlyIssuedBooks,
-          bookHistory: req.body.bookHistory,
-          allBooks: req.body.allBooks
+        dbConn.query("SELECT * FROM pending_approvals WHERE username = ?", [req.user.username], function (err, pendingResult) {
+          if (err) {
+            res.render('error', {
+              error: err
+            });
+            return;
+          }
+
+          dbConn.query("SELECT * FROM pending_approvals WHERE username = ? AND title = ? AND type = false", [req.user.username, req.body.title], function (err, result) {
+            if (err) {
+              res.render('error', {
+                error: err,
+                message: err.message
+              });
+              return;
+            }
+  
+            if (result.length > 0) {
+              res.render('view_books_user', {
+                allBooks: booksResult,
+                currentlyIssuedBooks: issuedResult,
+                bookHistory: historyResult,
+                pendingBooks: pendingResult,
+                message: 'You have already requested to return this book'
+              });
+            }
+            else{
+              dbConn.query("INSERT INTO pending_approvals (title, username, type) VALUES (?, ?, false)", [req.body.title, req.user.username], function (err, result) {
+                if (err) {
+                  res.render('error', {
+                    error: err,
+                    message: err.message
+                  });
+                  return;
+                }
+                res.render('view_books_user', {
+                  allBooks: booksResult,
+                  currentlyIssuedBooks: issuedResult,
+                  bookHistory: historyResult,
+                  pendingBooks: pendingResult,
+                  message: 'Book return requested successfully'
+                });
+              });
+            
+            }
+          }
+          );
+          
         });
       });
     });
   });
+  return;
 });
+
 
 
 module.exports = router;
