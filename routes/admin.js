3db8c8d5c678005e2441962d2abb13d10ac8dc7f
Aditya Jain <jainaditya0411@gmail.com>
2024-06-05 15:26:40 +0530

    add book update admin option

--- routes/admin.js
+++ routes/admin.js
@@ -4,103 +4,142 @@ var auth = require('../config/auth').admin;
 var dbConn = require('../config/db');
 
 router.get('/', auth, function (req, res, next) {
-  res.render('admin_home', {
-    username: req.user.username,
-  });
+    res.render('admin_home', {
+        username: req.user.username,
+    });
 });
 
 router.get('/books', auth, function (req, res, next) {
     dbConn.query("SELECT * FROM books", function (err, result) {
         if (err) {
-          res.render('error', {
-            error: err
-          });
-          return;
+            res.render('error', {
+                error: err
+            });
+            return;
         }
         res.render('book_catalogue', {
-          books: result
+            books: result
         });
-      });
+    });
 });
 
 
 
-      
+
 
 router.post('/add', auth, function (req, res, next) {
     var query = "INSERT INTO books (title, author, publisher) VALUES (?, ?, ?)";
     dbConn.query("SELECT * FROM books", function (err, books) {
-      if (err) {
-        res.render('error', {
-          message: err.message,
-          error: err
-
-        });
-        return;
-      }
-      if (req.body.title === '' || req.body.author === '' || req.body.publisher === '') {
-        res.render('book_catalogue', {
-          message: 'All fields are required',
-          books:books
-        });
-        return;
-      }
-      dbConn.query(query, [req.body.title, req.body.author, req.body.publisher], function (err, result) {
         if (err) {
-          res.render('book_catalogue', {
-            message: "A book with same title already added",
-            books: books
-          });
-          return;
+            res.render('error', {
+                message: err.message,
+                error: err
+
+            });
+            return;
         }
-        books.push({
-          title: req.body.title,
-          author: req.body.author,
-          publisher: req.body.publisher
+        if (req.body.title === '' || req.body.author === '' || req.body.publisher === '') {
+            res.render('book_catalogue', {
+                message: 'All fields are required',
+                books: books
+            });
+            return;
+        }
+        dbConn.query(query, [req.body.title, req.body.author, req.body.publisher], function (err, result) {
+            if (err) {
+                res.render('book_catalogue', {
+                    message: "A book with same title already added",
+                    books: books
+                });
+                return;
+            }
+            books.push({
+                title: req.body.title,
+                author: req.body.author,
+                publisher: req.body.publisher
+            });
+            books.sort(function (a, b) {
+                if (a.title < b.title) {
+                    return -1;
+                }
+                if (a.title > b.title) {
+                    return 1;
+                }
+                return 0;
+            });
+            res.render('book_catalogue', {
+                books: books,
+                message: 'Book added successfully'
+            });
         });
-        books.sort(function(a,b) {
-            if ( a.title < b.title ){
-                return -1;
-              }
-              if ( a.title > b.title ){
-                return 1;
-              }
-              return 0;
-        });
-        res.render('book_catalogue', {
-          books: books,
-          message: 'Book added successfully'
-        });
-      });
     });
-  });
+});
 
 router.post('/remove', auth, function (req, res, next) {
     var query = `DELETE FROM books WHERE title='${req.body.title}';`;
     dbConn.query("SELECT * FROM books", function (err, books) {
-      if (err) {
-        res.render('error', {
-          message: err.message,
-          error: err
+        if (err) {
+            res.render('error', {
+                message: err.message,
+                error: err
 
+            });
+            return;
+        }
+        dbConn.query(query, function (err, result) {
+            if (err) {
+                res.render('book_catalogue', {
+                    message: err.message,
+                    books: books
+                });
+                return;
+            }
+            books = books.filter(book => book.title !== req.body.title);
+            res.render('book_catalogue', {
+                books: books,
+                message: 'Book removed successfully'
+            });
+        });
+    });
+});
+
+router.post('/update', auth, function (req, res, next) {
+    res.render('update_book', {
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher
+    });
+});
+
+router.post('/update_confirm', auth, function (req, res, next) {
+    var query = `UPDATE books SET title='${req.body.title}', author='${req.body.author}', publisher='${req.body.publisher}' WHERE title='${req.body.old_title}'`;
+    if (req.body.title === '' || req.body.author === '' || req.body.publisher === '') {
+        res.render('update_book', {
+            message: 'All fields are required',
+            title: req.body.old_title,
+            author: req.body.old_author,
+            publisher: req.body.old_publisher
         });
         return;
-      }
-      dbConn.query(query, function (err, result) {
+    }
+    dbConn.query(query, function (err, result) {
         if (err) {
-          res.render('book_catalogue', {
-            message: err.message,
-            books: books
-          });
-          return;
+            res.render('update_book', {
+                message: err.message,
+                title: req.body.old_title,
+                author: req.body.old_author,
+                publisher: req.body.old_publisher
+            });
+            return;
         }
-        books = books.filter(book => book.title !== req.body.title);
-        res.render('book_catalogue', {
-          books: books,
-          message: 'Book removed successfully'
+        res.render('update_book', {
+            message: 'Book updated',
+            title: req.body.title,
+            author: req.body.author,
+            publisher: req.body.publisher
         });
-      });
     });
-  });
+});
+
 
 module.exports = router;
